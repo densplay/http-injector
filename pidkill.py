baa6d44e91a3dba3f2f80091b571620b14f949c5
JeelsBoobz <38210868+JeelsBoobz@users.noreply.github.com>
2021-09-08 05:25:39 +0700

    Update pidkill.py

--- pidkill.py
+++ pidkill.py
@@ -8,6 +8,8 @@ def handler():
     for line in output.splitlines():
         if target_process in str(line):
             pid = int(line.split()[0])
-            os.system(f'kill {pid}')
+            os.system(f'kill {pid} 2> /dev/null')
+            os.system(f'pkill screen 2> /dev/null')
+            os.system(f'screen -wipe 2> /dev/null')
 
 handler()
